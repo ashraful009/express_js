f5cd7e6373e968ccf09eaa55aa77f4effa38021e
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-07-29 22:25:15 +0600

    creating first API to get data

---
+++ REST_API/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "ashraful",
+        content: "Only coding is real",
+    },
+    {
+        username: "siam",
+        content: "i love bit mask",
+    },
+    {
+        username: "hasib",
+        content: "Only food is real",
+    },
+];
+
+
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+    
+});
+
+app.listen(port, () => {
+    console.log(`Listening port ${port}`);
+    
+})
