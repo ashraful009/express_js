0b96a2e0bab32fb160c5589f873923383d15e054
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-07-30 00:52:30 +0600

    creating 2nd API

--- REST_API/index.js
+++ REST_API/index.js
@@ -30,6 +30,17 @@ app.get("/posts", (req, res) => {
     
 });
 
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+    
+});
+
+app.post("/posts", (req, res) => {
+    let {username, content} = req.body;
+    posts.push({username, content});
+    res.send("post request working");
+})
+
 app.listen(port, () => {
     console.log(`Listening port ${port}`);
     
